89188138be0210f6052e5c80bcefe4fe080dbd67
u201817461 <u201817461@upc.edu.pe>
2023-09-21 22:25:57 -0500

    Fix: Testing Suites Auth

--- src/test/java/com/upc/leadyourway/integrationTest/AuthentificationIntegrationTest.java
+++ src/test/java/com/upc/leadyourway/integrationTest/AuthentificationIntegrationTest.java
@@ -2,6 +2,7 @@ package com.upc.leadyourway.integrationTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.upc.leadyourway.dto.AuthenticationResponse;
+import com.upc.leadyourway.dto.LoginRequest;
 import com.upc.leadyourway.dto.RegisterRequest;
 import com.upc.leadyourway.model.Roles;
 import org.junit.jupiter.api.Test;
@@ -9,14 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -29,11 +25,11 @@ public class AuthentificationIntegrationTest {
     public void testRegisterStudent() throws Exception {
         // Crear un objeto RegisterRequest con datos de prueba
         RegisterRequest registerRequest = RegisterRequest.builder()
-                .userFirstName("Nombre")
-                .userLastName("Apellido")
-                .userEmail("correo@example.com")
-                .userPassword("contraseña")
-                .userPhone("123456789")
+                .userFirstName("Alex")
+                .userLastName("Milton")
+                .userEmail("correalexo@example.com")
+                .userPassword("contraseña123")
+                .userPhone("12341235678")
                 .imageData("imagen")
                 .role(Roles.USER)
                 .build();
@@ -52,9 +48,28 @@ public class AuthentificationIntegrationTest {
         // Verificar el contenido de la respuesta
         String responseBody = mvcResult.getResponse().getContentAsString();
         AuthenticationResponse authenticationResponse = objectMapper.readValue(responseBody, AuthenticationResponse.class);
+    }
+    @Test
+    public void testLogin() throws Exception {
+        // Crear un objeto LoginRequest con datos de prueba
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUserEmail("correo@example.com");
+        loginRequest.setUserPassword("contraseña");
 
-        // Realiza las aserciones necesarias para verificar el resultado
+        // Convertir el objeto a JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(loginRequest);
 
-        // Otras aserciones según tu lógica de negocio
+        // Realizar una solicitud POST al endpoint /api/leadyourway/v1/auth/login
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/leadyourway/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk()) // Espera una respuesta HTTP 200 OK
+                .andReturn();
+
+        // Verificar el contenido de la respuesta
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        // Aquí puedes analizar el cuerpo de la respuesta JSON para verificar que el inicio de sesión fue exitoso.
+        // Por ejemplo, puedes verificar si el token de acceso se encuentra en la respuesta.
     }
 }
